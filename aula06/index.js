9fb3077b6d2387ec215f9a5cae32b6d21ef326c6
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-06 15:04:05 -0300

    conceitos de variáveis
    aprendemos alguns conceitos de variáveis let

---
+++ aula06/index.js
@@ -0,0 +1,16 @@
+let nome = 'Rauje'
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com joão em 2015.');
+console.log('O filho de jão se chama Eduardo');
+
+//Não podemos criar variáveis com palavras reservadas.
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// não utilise var, utilize let
