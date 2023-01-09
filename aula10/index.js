44aa055e7da1f175ac39320496cc2d4237f26e77
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-09 16:17:28 -0300

    operadores lógicos
    operadores lógicos

---
+++ aula10/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores aritimétiocs
++ adição / concatenação
+- subtração
+* multiplicação
+/ divisão
+** petenciação
+% resto da divisão
+ */
+
+const num1 = 10;
+const num2 = 5;
+
+console.log( num1 / num2);
