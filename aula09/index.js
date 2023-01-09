4649db7b4049460733ece114340008d7fd2e7bde
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-09 14:26:42 -0300

    const
    aprendendo sobre variáveis

--- aula09/index.js
+++ aula09/index.js
@@ -1,16 +1,10 @@
-var nome = 'rauje';
-var nome = 'rhianzzi';
-let umNome = 'rauje';
+const nome = 'Rauje'; //string
+const nome1 = "rauje"; //string
+const nome2 = `Rauje`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória
+const aprovado = true; // Boolean = true ou false (Lógico)
 
-//diferança de var e let, var eu posso redeclarar a mesma variável, ja com let não posso fazer o mesmo.
-
-
-console.log(nome);
-
-
-
-
-
-
-
-
+console.log(typeof num1, num1)
