aad88cef468ff679bf12a1b7760f4deb8d2957a4
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-10 15:00:30 -0300

    Strings
    Aula sobre strings

---
+++ aula11/script.js
@@ -0,0 +1,15 @@
+/*alert('Com a nossa menssagem!');
+confirm('deseja realmente apagar ?');
+prompt('Digite seu nome.');*/
+
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número.');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2
+
+alert(`A soma dos numero digitados é ${resultado}.`);
+
