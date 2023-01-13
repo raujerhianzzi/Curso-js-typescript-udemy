56db30d50d4220601f88fc21d2e7e12f79e3e9e3
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-13 10:07:52 -0300

    exercício

--- aula15/js/script.js
+++ aula15/js/script.js
@@ -1,5 +1,15 @@
-const  numero = Number(prompt('Digite um numero:'));
+const  numero = Number(prompt('Digite um numero com várias casas décimais usando ponto.:'));
 const numeroTitulo = document.getElementById('Numero titulo');
 const texto = document.getElementById('texto');
+
+
+numeroTitulo.innerHTML = numero
+texto.innerHTML += `Raiz quadrada: <strong>${numero ** 0.5}</strong> <br />`
+texto.innerHTML += `${numero} é intero: ${numero} <br />`
+texto.innerHTML += `É NaN: ${numero} <br />`
+texto.innerHTML += `Arredondando para baixo: ${Math.floor(numero)} <br />`
+texto.innerHTML += `Arredondando para cima: ${Math.ceil(numero)} <br />`
+texto.innerHTML += `Com duas casas décimais: ${Math.trunc(2-numero)} <br />`
+
 
 
