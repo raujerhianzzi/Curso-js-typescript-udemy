66c22fe178d3c7bd7bcf365ee678c663bc865bb0
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-16 14:37:45 -0300

    resolvendo exercício
    resolução

--- aula15/js/script.js
+++ aula15/js/script.js
@@ -5,11 +5,11 @@ const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero
 texto.innerHTML += `Raiz quadrada: <strong>${numero ** 0.5}</strong> <br />`
-texto.innerHTML += `${numero} é intero: <strong>${numero}</strong> <br />`
-texto.innerHTML += `É NaN: <strong>${numero}</strong> <br />`
+texto.innerHTML += `${numero} é intero: <strong>${Number.isInteger(numero)}</strong> <br />`
+texto.innerHTML += `É NaN: <strong>${Number.isNaN(numero)}</strong> <br />`
 texto.innerHTML += `Arredondando para baixo: <strong>${Math.floor(numero)}</strong> <br />`
 texto.innerHTML += `Arredondando para cima: <strong>${Math.ceil(numero)}</strong> <br />`
-texto.innerHTML += `Com duas casas décimais: <strong>${Math.trunc(2-numero)}</strong> <br />`
+texto.innerHTML += `Com duas casas décimais: <strong>${numero.toFixed(2)}</strong> <br />`
 
 
 
