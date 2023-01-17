1a97d5923986e4e70326a19e8a8c2950998f569d
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-17 16:01:01 -0300

    aula arrays
    arrays básico

---
+++ aula16/index.js
@@ -0,0 +1,7 @@
+const alunos = ['luiz', 'maria', 'joão'];
+//console.log(alunos.length);
+alunos.unshift('carla'); //adiiona no inicio da arrays
+alunos.push('rauje'); //adiona um elemnto ao final da arrays
+alunos.pop('rauje'); //retira um elemento 
+
+console.log(alunos);
