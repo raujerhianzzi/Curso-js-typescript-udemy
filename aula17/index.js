bc514b4ac6454b80c326efb07f365b88557a2b1b
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-19 14:35:17 -0300

    funções
    aprendemos como cria function

--- aula17/index.js
+++ aula17/index.js
@@ -1,3 +1,24 @@
-const alunos = ['mario', 'rauje', 'pedro', 'sofia']
+/*function saudacao(nome) {
+    console.log(`Bom dia ${nome}`);
 
-console.log(alunos);
+}
+
+saudacao('Rauje');*/
+
+//function soma(x, y) {
+//    const resultado = x + y;
+//    return resultado;
+//}
+
+//console.log(soma(2, 2));
+
+/*const raiz = function(n) {  
+    return n ** 0.5;
+};*/
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+
+
