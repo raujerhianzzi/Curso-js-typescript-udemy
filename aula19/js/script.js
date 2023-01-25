6444c2fef13de0b53be435bdc715c5f57e4cdd34
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-25 16:59:23 -0300

    exercicio

--- aula19/js/script.js
+++ aula19/js/script.js
@@ -1,15 +1,32 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    
+    const pessoas = [];
+    
+    function recebeEventoForm (evento){
+        evento.preventDefault();
 
-    form.onsubmit = function (evento) {
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
 
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value 
+        });
 
-        
-        //evento.preventDefault();
-       // alert(casa);
-       // console.log('foi enviado.');
+        console.log(pessoas);
 
-    };
+        resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+   
 }
+meuEscopo();
 
 
