7a313483f1129ba4b5c05472e6aa8c9e4b26c3e4
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-01-26 14:53:41 -0300

    operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação
+> mmaior que
+>= maior que igual a 
+< menor que
+<= menor que igual a 
+== igualdade (valor)
+=== igualdade estritra (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+ */
+
